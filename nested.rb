d2b3be4e51124e8b675bd04fc89aec85bca1f421
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-11 03:19:52 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -83,7 +83,7 @@ programmer_hash =
 
 programmer_hash{:yukihiro_matsumoto => {
           :known_for => "Ruby",
-          :languages => ["LISP", "C"]
+          :languages => "LISP", "C"
         }
     
 end
