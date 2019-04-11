94786a1245499e285565193f0a92130259ea1099
Jesse Barnes <jessekathrynbarnes@gmail.com>
2019-04-11 03:24:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,10 @@ programmer_hash =
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] =":known_for"] = "Ruby",
-          [:languages] = "LISP", "C"
-      
+programmer_hash[:yukihiro_matsumoto] ={
+          :known_for => "Ruby",
+          :languages => ["LISP", "C"]
+        }
 end
 
 def changing_alan
